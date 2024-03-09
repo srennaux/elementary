77793d6c0e131a7d548e8c88df8b24398e427914
Сергей <serge.tori@gmail.com>
2024-03-09 13:22:48 +0300

    1.9 Конвертер валюты

---
+++ src/main/java/ru/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 100;
+        return rsl;
+    }
+    public static float rubleToDollar(float value) {
+        float rsl = value / 90;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float dollar = rubleToDollar(300);
+        float euro = rubleToEuro(300);
+        System.out.println("300 rubles are " + dollar);
+        System.out.println("300 rubles are " + euro);
+    }
+
+}
+
